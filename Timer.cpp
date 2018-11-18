ad9ef117a7138410f4216fad846bce6a6c1bd2de
zinjkov <zinjkov.su@gmail.com>
2018-11-19 02:13:59 +1000

    added code review

--- Timer.cpp
+++ Timer.cpp
@@ -14,7 +14,7 @@ void Timer::start()
 
 long Timer::elapsed()
 {
-	if (m_isStarted) return millis() - m_start;
+	if (m_isStarted) return millis() - m_start; // сделать через тернарный оператор 
 	return 0;
 }
 
